53f8a1a24a6a96809f5811479ef6ef3282d237ad
Dharmelolar <hardeydoyeen1@gmail.com>
2022-10-26 10:56:30 +0100

    edit yml file

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>CI/CD Pipeline with Netlify and Github Actions</h2>
-        <p>My test passed</p>
-        <p>Testing again</p>
-        <p>failer </p>
-        <p>please work</p>
-        <p>abeg now</p>
+        
       </header>
     </div>
   );
