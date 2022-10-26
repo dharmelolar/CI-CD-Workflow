6e62ed7902ec5e42fc4fa8e6bcdcef5c5a66feed
Dharmelolar <hardeydoyeen1@gmail.com>
2022-10-26 09:47:10 +0100

    omo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>CI/CD Pipeline with Netlify and Github Actions</h2>
+        <p>My text passed</p>
       </header>
     </div>
   );
